165bc736a8944b20ae7055e305522a22893e673d
nicolasdca <nicolasdavid193@gmail.com>
2024-06-10 17:06:34 -0300

    API Rest YATA

---
+++ yata-todo-app/src/main/java/br/com/nicolas/yata_todo_app/YataTodoAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.nicolas.yata_todo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YataTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YataTodoAppApplication.class, args);
+	}
+
+}
---
+++ yata-todo-app/src/test/java/br/com/nicolas/yata_todo_app/YataTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nicolas.yata_todo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YataTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
